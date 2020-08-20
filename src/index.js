abfa37f1a8f3aff520f7005bdc706efd7b907fe2
jan-far <ligalifarouk@gmail.com>
2020-08-20 21:37:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import debug from "debug";
 import config from "./config";
 
 let app = express();
-const port = parseInt(process.env.PORT || "3000");
+const port = parseInt(process.env.PORT || "3000", 10);
 
 app = config(app);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Agro-Mart!')
+})
+
 app.listen(port, () => {
- debug(`Server running on port ${port}`);
+    debug(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 export default app;
