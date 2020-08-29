29767edab8ba28a379b8fefd0fff0b3db7ef6310
Kingsley Victor <javaprodigy56@gmail.com>
2020-08-29 09:54:40 -0400

    Add more controller function

--- src/controller/farm.js
+++ src/controller/farm.js
@@ -1,4 +1,5 @@
 import { Farm } from "../db";
+import { ErrorResponse } from "../custom";
 
 export class FarmController {
  static async add(req, res) {
@@ -20,6 +21,57 @@ export class FarmController {
    // Send response
    res.status(201).json({
     statusCode: 201,
+    response
+   });
+  } catch (error) {
+   res.status(500).json({
+    statusCode: 500,
+    response: error.message
+   });
+  }
+ }
+
+ static async getFarmByAuthenticatedUser(req, res) {
+  try {
+   // Get all farms
+   const farms = await Farm.findByUserId(req.payload._id);
+
+   // Find a single farm using path parameter
+   const farm = farms.find((f) => f._id === req.params.id);
+
+   // Throw error if farm with specified id is not found
+   if (!farm)
+    throw new ErrorResponse(404, "Farm details not found");
+
+   // API response
+   const response = {
+    ...farm.toJSON()
+   };
+
+   // Send response
+   res.status(200).json({
+    statusCode: 200,
+    response
+   });
+  } catch (error) {
+   res.status(error.c || 500).json({
+    statusCode: error.c || 500,
+    response: error.message
+   });
+  }
+ }
+
+ static async getAllFarmsByAuthenticatedUser(req, res) {
+  try {
+   // Get all farms using user id from decoded payload
+   const farms = await Farm.findByUserId(req.payload._id);
+
+   // API response
+   const response = farms.map((f) => f.toJSON());
+
+   // Send response
+   res.status(200).json({
+    statusCode: 200,
     response
    });
   } catch (error) {
--- src/routes/farm.js
+++ src/routes/farm.js
@@ -5,5 +5,7 @@ import { Auth } from "../middlewares";
 const router = Router();
 
 router.post("/add", Auth.hasToken, Auth.hasRole("farm"), FarmController.add);
+router.get("/get/:id", Auth.hasToken, Auth.hasRole("farm"), FarmController.getFarmByAuthenticatedUser);
+router.get("/all", Auth.hasToken, Auth.hasRole("farm"), FarmController.getAllFarmsByAuthenticatedUser);
 
 export default router;
